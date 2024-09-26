e45379207690af2c0ff785d229ec0ded9270c8ca
dinesh-gms <dineshg4127@gmail.com>
2024-09-26 13:42:14 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import { Fragment } from "react";
+import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+
+const HomePage = (props) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetup</title>
+        <meta
+          name="description"
+          content="This is a react meetup page is highly interactive with user meetups."
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+};
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://dineshcena257:test-123@cluster0.02p7s7a.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const meetups = await meetupCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
